68823394d0379751ce5074d741ef34a971201424
Solomon Sefa (sask) <kwabena81@yahoo.com>
2022-10-07 04:25:48 -0400

    more coding

--- EtrJavaDev/src/main/java/com/sas/Main.java
+++ EtrJavaDev/src/main/java/com/sas/Main.java
@@ -1,19 +1,35 @@
 package com.sas;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException, IOException {
         readLocations();
     }
 
-    private static void readLocations() {
+    private static void readLocations() throws URISyntaxException, IOException {
         String fileString = null;
-        String file = "";
-        fileString = new String();
+        String jsonFile = "interchanges.json";
+        URL resource = Main.class.getClassLoader().getResource(jsonFile);
+        byte[] bytes = Files.readAllBytes(Paths.get(resource.toURI()));
+        fileString = new String(bytes);
+
+        if (fileString != null)
+            print(fileString);
+
         LocationNode locationNode0 =
                 createLocationNode(1, "QEW", 43.484361d, -79.766037d);
         LocationNode locationNode2 =
                 createLocationNode(2, "Dundas Street", 43.383554d, -79.833478d);
         computeDistanceAndCharge(locationNode0, locationNode2);
+    }
+
+    private static void print(String fileString) {
+        System.out.println("jason file: " + fileString);
     }
 
     public static void computeDistanceAndCharge(LocationNode locationNode1, LocationNode locationNode2) {
