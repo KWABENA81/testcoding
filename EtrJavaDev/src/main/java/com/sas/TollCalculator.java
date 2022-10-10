68bc672845a8d5dc3bb75badb5927bbe7ccc1265
Solomon Sefa (sask) <kwabena81@yahoo.com>
2022-10-10 02:09:34 -0400

    more additions

--- EtrJavaDev/src/main/java/com/sas/TollCalculator.java
+++ EtrJavaDev/src/main/java/com/sas/TollCalculator.java
@@ -1,6 +1,7 @@
 package com.sas;
 
 import com.sas.core.JSONHandler;
+import com.sas.model.LocationNode;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
@@ -12,17 +13,20 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TollCalculator {
     private static Map<Integer, Object> handlerLocationsMap;
+    private static Set<LocationNode> locations;
+    private static Scanner inputScanner;
+    private static String exitLocationName;
+    private static String startLocationName;
 
     public static void main(String... args) throws URISyntaxException, IOException {
         readLocations(args);
-        String str = "Please enter Trip start location & destination:\n(Comma Separated)";
-
-        menuPrompt(str);
+        inputScanner = new Scanner(System.in);
+        menuPrompt();
     }
 
     private static void readLocations(String... args) throws URISyntaxException {
@@ -45,28 +49,106 @@ public class TollCalculator {
         }
     }
 
-    private static void menuPrompt(String str) throws IOException {
-        long pid = 0l;
-        String inputString = null;
-        Scanner inputScanner = null;
-        try {
-            inputScanner = new Scanner(System.in);
-            boolean canProceed = true;
-            while (canProceed) {
-                printout(str);
-                inputString = inputScanner.nextLine();
-                printout(inputString);
-                pid = checkpid();
-                
-                canProceed = false;
-            }
-        } catch (IOException e) {
+    private static void menuPrompt() throws IOException {
+        String str = "______________________\nToll calculator prompt.\n" + "Select one:\nN - Name Search:\n" + "S - Start Entrance:\nX - End Exit:\nF - Finish: \n";
 
+        long pid = 0L;
+        String options = "Exit";
+        String inputString = null;
+
+        boolean canExit = !options.equalsIgnoreCase("EXIT");
+
+        while (!canExit) {
+            printout(str);
+            inputString = inputScanner.nextLine();
+            // printout(inputString);
+            switch (inputString) {
+                case "N":
+                case "n":
+                    optionSearch();
+                    break;
+                case "X":
+                case "x":
+                    optionHwyExit();
+                    break;
+                case "S":
+                case "s":
+                    optionHwyStart();
+                    break;
+                case "F":
+                case "f":
+                    //canExit = true;
+                    //exit(0);
+            }
+            //  pid = checkpid();
+
+//                   List<String> slist = locations.stream().map(l -> l.getLocationName())
+//                           .sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+//            slist.forEach((l -> printout(l)));
+            // printout(obj.toString());
+            //;entrySet()){
+            //obj=list.
+            // }//stream().flatmap(LocationNode::getLocationName).
+            // collect(Collectors.toList());
+            canExit = false;
         }
     }
 
+    private static void optionHwyStart() {
+        printout("\nEnter HWY Start: ");
+        String startString = inputScanner.nextLine();
+        Optional<String> locationOptional = locations.stream().map(l -> l.getLocationName())
+                .filter(s -> s.equalsIgnoreCase(startString)).findFirst();
+        startLocationName = (locationOptional.isPresent()) ? locationOptional.get() : null;
+        if (startLocationName == null) {
+            optionSearch();
+        } else {
+            //printout(startLocationName);
+            optionHwyExit();
+        }
+    }
+
+    private static void optionSearch() {
+        exitLocationName = null;
+        startLocationName = null;
+        printout("Enter Name to Search: ");
+        String searchString = inputScanner.nextLine();
+        if (searchString.length() < 3) return;
+        List<String> slist = locations.stream().map(l -> l.getLocationName())
+                .filter(s -> s.toLowerCase().contains(searchString.toLowerCase()))
+                .sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+
+        if (slist.isEmpty()) {
+            optionSearch();
+        } else {
+            slist.forEach(x -> printout(x));
+            optionHwyStart();
+        }
+    }
+
+    private static void optionHwyExit() {
+        printout("\nEnter HWY Exit: ");
+        String exitString = inputScanner.nextLine();
+        Optional<String> locNode = locations.stream().map(l -> l.getLocationName())
+                .filter(s -> s.equalsIgnoreCase(exitString)).findFirst();
+        exitLocationName = (locNode.isPresent()) ? locNode.get() : null;
+        if (exitLocationName == null) {
+            optionHwyExit();
+        } else {
+            //printout(exitLocationName);
+            optionTollCompute();
+        }
+    }
+
+    private static void optionTollCompute() {
+        printout("\nHwy Entrance: "+startLocationName);
+        printout("Hwy Exit:\t"+exitLocationName);
+        printout("Distance:\t");
+        printout("Cost:\t\t");
+    }
+
     private static long checkpid() throws IOException {
-        long pid = 0l;//process.pid();
+        long pid = 0L;//process.pid();
         Runtime.getRuntime().exec("kill " + pid);
         return pid;
     }
@@ -97,6 +179,7 @@ public class TollCalculator {
         JSONHandler jsonHandler = new JSONHandler();
         jsonHandler.parseJSONObject(jsonObj);
         handlerLocationsMap = jsonHandler.getLocationsMap();
+        locations = jsonHandler.getLocationNodes();
     }
 
 }
